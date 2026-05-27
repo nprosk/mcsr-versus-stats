b99ed22e8e1a5470c16d9db3a7011436ea7d3972
Nico Proskauer Valerio <nicoprosk@gmail.com>
2026-05-26 22:07:16 -0700

    get rid of existing css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
-import './App.css'
 import Home from './components/Home';
 import PlayerVersusPage from './components/PlayerVersusPage';
+import { TestPlayerPage } from './components/TestPlayerPage';
 
 function App() {
 
    return (
       <Routes>
          <Route path="/" element={<Home />} />
-         <Route path="/:identifier" element={<PlayerVersusPage />} />
+         <Route path="/user/:identifier" element={<PlayerVersusPage />} />
+         <Route path="/test" element={<TestPlayerPage />} />
       </Routes>
    )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
+import { MantineProvider } from '@mantine/core'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,9 +18,11 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter basename='/mcsr-versus-stats'>
-        <App />
-      </BrowserRouter>
+      <MantineProvider>
+        <BrowserRouter basename='/mcsr-versus-stats'>
+          <App />
+        </BrowserRouter>
+      </MantineProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
