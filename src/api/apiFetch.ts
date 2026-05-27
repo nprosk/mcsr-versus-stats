90e07376e3071fcd357c0f4f3c2f3fd78ba9ec26
Nico Proskauer Valerio <nicoprosk@gmail.com>
2026-05-26 21:14:52 -0700

    add fetch function from minotar

--- src/api/apiFetch.ts
+++ src/api/apiFetch.ts
@@ -1,4 +1,4 @@
-import { rankedApiBaseUrl } from "../constants";
+import { minotarBaseUrl, rankedApiBaseUrl } from "../constants";
 
 const apiFetch = async <T>(url: string): Promise<T> => {
   const res = await fetch(url);
@@ -8,5 +8,10 @@ const apiFetch = async <T>(url: string): Promise<T> => {
 
 export const mcsrRankedApiFetch = async <T>(url: string): Promise<T> => {
   const { data } = await apiFetch<{ status: string; data: T }>(rankedApiBaseUrl + url);
+  return data;
+};
+
+export const minotarFetch = async <T>(url: string): Promise<T> => {
+  const { data } = await apiFetch<{ status: string; data: T }>(minotarBaseUrl + url);
   return data;
 };
--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const rankedApiBaseUrl = "https://api.mcsrranked.com";
+export const minotarBaseUrl = "https://minotar.net/avatar";
 
 export const maxApiCallsByOneFunction = 20;
