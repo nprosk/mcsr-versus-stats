b8a65550005865dcea2dd2b8eb38e9c2a83eb49d
Nico Proskauer Valerio <nicoprosk@gmail.com>
2026-06-06 02:41:25 -0700

    put divider between opponents

--- src/components/ManyOpponents.tsx
+++ src/components/ManyOpponents.tsx
@@ -1,12 +1,20 @@
-import { Stack } from "@mantine/core"
+import { Divider, Stack } from "@mantine/core"
 import type { MatchInfo, UserIdentifier } from "../types/mscrRankedObjects"
 import { OneOpponent } from "./OneOpponent";
+import { Fragment } from "react/jsx-runtime";
 
 
 export const ManyOpponents = ({ player, oppMatches }: { player: UserIdentifier, oppMatches: Record<string, MatchInfo[]> }) => {
     return (
-        <Stack>
-            {Object.entries(oppMatches).map(([opp, matches]) => <OneOpponent key={opp} player={player} opponent={opp} matches={matches} />)}
+        <Stack gap="md">
+            {Object.entries(oppMatches).map(([opp, matches], i) => {
+                return (
+                    <Fragment key={opp}>
+                        {i > 0 && <Divider />}
+                        <OneOpponent player={player} opponent={opp} matches={matches} />
+                    </Fragment>
+                )
+            })}
         </Stack>
     );
 }
