118949078ac3927762cde55feeb369986d762a80
Nico Proskauer Valerio <nicoprosk@gmail.com>
2026-06-06 03:04:23 -0700

    fix player head and text for no opp case

--- src/components/OneOpponent.tsx
+++ src/components/OneOpponent.tsx
@@ -6,10 +6,11 @@ import { MatchTimes } from "./MatchTimes";
 export const OneOpponent = ({ player, opponent, matches }: { player: UserIdentifier; opponent: UserIdentifier; matches: MatchInfo[] }) => {
     console.log("Rendering OneOpponent", player, opponent, matches);
     const opponentNickname = matches[0].players.find(p => p.uuid === opponent)?.nickname || opponent;
+    const playerNickname = matches[0].players.find(p => p.uuid === player)?.nickname || player;
     return (
         <Grid columns={12}>
-            <Grid.Col span={2}><Center><PlayerHead identifier={opponent} /></Center></Grid.Col>
-            <Grid.Col span={2}><Center><Text>{opponentNickname}</Text></Center></Grid.Col>
+            <Grid.Col span={2}><Center><PlayerHead identifier={opponent === "NO_OPPONENT" ? player : opponent} /></Center></Grid.Col>
+            <Grid.Col span={2}><Center><Text>{opponent === "NO_OPPONENT" ? playerNickname : opponentNickname}</Text></Center></Grid.Col>
             <Grid.Col span={8}><MatchTimes player={player} matches={matches} /></Grid.Col>
         </Grid>
     );
--- src/components/PlayerHead.tsx
+++ src/components/PlayerHead.tsx
@@ -1,5 +1,7 @@
 import { Image } from "@mantine/core"
 
 export const PlayerHead = ({ identifier }: { identifier: string }) => {
-    return <Image src={`https://api.mineatar.io/face/${identifier}?scale=24`} fallbackSrc="https://api.mineatar.io/face/unknown?scale=24" w={48}></Image>;
+    return (
+        <Image src={`https://api.mineatar.io/face/${identifier}?scale=24`} fallbackSrc="https://minotar.net/avatar/MHF_Steve" w={48}></Image>
+    );
 }
