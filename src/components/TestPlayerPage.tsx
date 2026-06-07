4e660f4c74a905c01829cf9af6ec611a0d2f2d61
Nico Proskauer Valerio <nicoprosk@gmail.com>
2026-06-07 02:16:16 -0700

    fix errors to make deploy work hopefully

--- src/components/TestPlayerPage.tsx
+++ src/components/TestPlayerPage.tsx
@@ -6,18 +6,12 @@ import { Container } from "@mantine/core";
 export const TestPlayerPage = () => {
     const groupedMatches = getTestGroupedMatches();
 
-    const slickUuidQuery = useProfileFromNickname("Slickburrito735");
-    const slickUuid = slickUuidQuery.data?.id;
-    const nproskUuidQuery = useProfileFromNickname("Nprosk");
+    const nproskUuidQuery = useProfileFromNickname("nprosk");
     const nproskUuid = nproskUuidQuery.data?.id;
-    const qwertyUuidQuery = useProfileFromNickname("Qwertycube10");
-    const qwertyUuid = qwertyUuidQuery.data?.id;
-    const twoOpps = { [slickUuid]: groupedMatches[slickUuid], [qwertyUuid]: groupedMatches[qwertyUuid] };
-    console.log("twoOpps", twoOpps);
 
     return (
         <Container fluid p="md">
-            <ManyOpponents player={nproskUuid} oppMatches={groupedMatches} />
+            {nproskUuid && <ManyOpponents player={nproskUuid} oppMatches={groupedMatches} />}
         </Container>
     )
 }
