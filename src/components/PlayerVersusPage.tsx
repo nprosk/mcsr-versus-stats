2b73e5c8d5133d6638ed36127453b4e99398a64b
Nico Proskauer Valerio <nicoprosk@gmail.com>
2026-06-09 00:09:21 -0700

    basic version of player versus page

--- src/components/PlayerVersusPage.tsx
+++ src/components/PlayerVersusPage.tsx
@@ -1,27 +1,24 @@
 import { useParams } from "react-router-dom";
 import { useAllUserMatches } from "../hooks/useAllUserMatches";
+import { useProfileFromNickname } from "../hooks/useProfileFromNickname";
+import { ManyOpponents } from "./ManyOpponents";
+import { groupMatchesByOpponent } from "../util/groupMatchesByOpponent";
+import { Container } from "@mantine/core";
 
 export default function PlayerVersusPage() {
     const { identifier } = useParams<{ identifier: string }>();
 
-    const { data, isLoading, error } = useAllUserMatches(identifier!);
+    const { data } = useAllUserMatches(identifier!);
+    const nproskUuidQuery = useProfileFromNickname(identifier!);
+    const nproskUuid = nproskUuidQuery.data?.id;
 
-    console.log("total matches for user:", data?.length);
-    console.log("User matches data:", data);
-    console.log("Loading state:", isLoading);
-    console.log("Error state:", error);
 
     return (
-        <div>
+        <Container fluid p="md">
             <div>Player Versus Page for {identifier}</div>
-            {isLoading && <div>Loading matches...</div>}
-            {error && <div>Error loading matches: {error.message}</div>}
-            {data && (
-                <div>
-                    <h2>Total Matches: {data.length}</h2>
-                    <div>{JSON.stringify(data)}</div>
-                </div>
+            {(!data || !nproskUuid) && <div>Loading matches...</div>}
+            {data && nproskUuid && (<ManyOpponents player={nproskUuid} oppMatches={groupMatchesByOpponent(nproskUuid, data)} />
             )}
-        </div>
+        </Container>
     );
 }
